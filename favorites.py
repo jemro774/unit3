4fd563e5cd4c32091461aa9142718de2f0da417d
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-27 13:54:00 -0400

    Updated from Brython Server: 9/27/2017 1:54:00 PM

--- favorites.py
+++ favorites.py
@@ -1,1 +1,12 @@
+#Jack Robey
+#9/27/17
+#favorites.py - asks user for their favorite word and number
+
+favWord = input('Enter your favorite word: ')
+num = int(input('Enter your favorite number: '))
+
+i = 1
+while i <= num:
+    print(favWord)
+    i += 1
 
