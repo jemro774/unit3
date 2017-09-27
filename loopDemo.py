3692a2ca0741917109074d3a1d831b92aaaf3841
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-27 13:48:46 -0400

    Updated from Brython Server: 9/27/2017 1:48:45 PM

--- loopDemo.py
+++ loopDemo.py
@@ -7,7 +7,8 @@
 for i in range(1,6):
     print('I love Comuter Science')"""
 
-"""    
+""" 
+#print I love computer science 5 times
 i = 1
 while i <= 5:
     print('I love Comuter Science')
@@ -19,6 +20,7 @@ for i in range(1,21):
     print(i)"""
 
 """
+#print the numbers from 1 to 20
 i = 1
 while i <= 20:
     i = i+1"""
@@ -28,11 +30,6 @@ while i <= 20:
 for i in range(13,18):
     if i%2==1:
         print(i)"""
-        
-i = 13
-while i<=27:
-    print(i)
-    i = i + 2
 
 """
 #print out the odd numbers from 13 to 27 (option 2)
@@ -41,8 +38,23 @@ for i in range(13,18,2): #go up by 2 each time
         print(i)"""
 
 """
+#print out the odd numbers from 13 to 27
+i = 13
+while i<=27:
+    print(i)
+    i = i + 2"""
+
+"""
 #add up the numbers from 1 to 5
 total = 0
 for i in range(1,6):
     total = total + i
 print(total)"""
+
+"""
+#add up the numbers from 1 to 5
+total = 0
+i = 1
+while i <= 5:
+    total += i #total = total + i
+    i += 1 #mean i = i+1"""
