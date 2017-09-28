1f964aea999ba338dc324d797b1da07a9af08f38
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-28 14:38:12 -0400

    Updated from Brython Server: 9/28/2017 2:38:11 PM

--- squares.py
+++ squares.py
@@ -1,1 +1,10 @@
+#Jack Robey
+#9/28/17
+#squares.py - prints out number of square numbers the user enters in
 
+num = int(input('Enter a number: '))
+
+i = 1
+while i <= num:
+    print(i**2)
+    i = i + 1
