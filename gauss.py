09d62df04138b9efa41ac578c00360d5bd9aa058
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-29 14:56:49 -0400

    Updated from Brython Server: 9/29/2017 2:56:48 PM

--- gauss.py
+++ gauss.py
@@ -2,7 +2,11 @@
 #9/29/17
 #gauss.py - uses a loop to add up all the numbers from 1 to 100
 
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+diff = int(input('Enter a difference: '))
+
 total = 0
-for i in range(1,101):
+for i in range(num1,num2+1,diff):
     total += i
 print(total)
