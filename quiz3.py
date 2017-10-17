4962998f411dbc59ed6814f03d3ef6c9e13826f1
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-17 14:58:30 -0400

    Updated from Brython Server: 10/17/2017 2:58:30 PM

--- quiz3.py
+++ quiz3.py
@@ -2,6 +2,7 @@
 #10/17/17
 #quiz3.py
 
+
 for i in range(-5, 6):
     print(i)
     
@@ -15,5 +16,11 @@ for i in range(13,332,2):
     total = total + i
 print(total)
 
-
+while True:
+    word = input('Enter a word: ')
+    print(word)
+    if 'z' in word:
+        break
+            
+    
 
