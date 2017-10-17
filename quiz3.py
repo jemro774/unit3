bf62dc67e405d046fcbd9ae203ae62665b68bd48
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-10-17 14:52:21 -0400

    Updated from Brython Server: 10/17/2017 2:52:21 PM

--- quiz3.py
+++ quiz3.py
@@ -1,1 +1,19 @@
+#Jack Robey
+#10/17/17
+#quiz3.py
+
+for i in range(-5, 6):
+    print(i)
+    
+i = 18
+while i <= 32:
+    print(i)
+    i = i + 2
+
+total = 0
+for i in range(13,332,2):
+    total = total + i
+print(total)
+
+
 
